0a72275b1e6687afa1b87d92cc1e45f98fe1a439
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-22 18:02:32 +0300

    Add User and Repository classess

---
+++ src/app/repository.ts
@@ -0,0 +1,4 @@
+export class Repository {
+  constructor(public avatar_url:string,public name:string, public url:string,public public_repo:number,
+    public created_at: Date){}
+}
---
+++ src/app/user.ts
@@ -0,0 +1,4 @@
+export class User {
+  constructor(public_repos:number,
+    public followers:number, public following:number,public created_at: Date){}
+}
