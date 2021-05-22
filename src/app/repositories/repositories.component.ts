d2b33e7c0da18e9acf47b4b7f438e0ec4e611883
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-22 17:42:03 +0300

    Update repository component

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/github.service';
+
 
 @Component({
   selector: 'app-repositories',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  profiles: any;
+  repos: any;
+  username:string;
+
+  constructor(private profileService: GithubService) { }
 
   ngOnInit(): void {
   }
+  findProfiles(){
+  	this.profileService.updateProfile(this.username);
+  	// tslint:disable-next-line: indent
+  	// tslint:disable-next-line: indent
+  	this.profileService.getProfileInfo().subscribe(profile => {
+  		console.log(profile);
+  		// tslint:disable-next-line: indent
+  		this.profiles = profile;
+  	// tslint:disable-next-line: indent
+  	});
+
+  	this.profileService.getProfileRepos().subscribe(repos => {
+  		console.log(repos);
+  		this.repos = repos;
+  	})
+  }
+
 
 }
