6c345c2f9aa169980cff1c835e3756058375c72f
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-22 15:57:08 +0300

    Add github service

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../services/github.service';
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css']
+  styleUrls: ['./github.component.css'],
+  providers:[GithubService]
 })
 export class GithubComponent implements OnInit {
-
-  constructor() { }
-
+  user:any;
+  repos:any;
+  username:string;
+  
+  constructor(private _githubService:GithubService){
+      console.log('Github Component Init...');   
+  }
+  
+  search(){
+      this._githubService.updateUsername(this.username);
+      
+      this._githubService.getUser().subscribe(user => {
+          //console.log(user);
+          this.user = user;
+      });
+      
+      this._githubService.getRepos().subscribe(repos => {
+          this.repos = repos;
+      });
+  }
   ngOnInit(): void {
   }
 
